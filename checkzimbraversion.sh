9a62991a81a3795d29976cf2cb291fc1e39e64ab
jorgedlcruz <jorgedlcruz@gmail.com>
2017-10-06 12:41:42 +0100

    Added CentOS/Ubuntu check

--- checkzimbraversion.sh
+++ checkzimbraversion.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
-dpkg -s zimbra-core | awk -F"[ ',]+" '/Version:/{print $2}' | awk -F. '{print $1"."$2"."$3" "$4}'
+if [ -f /etc/redhat-release ]; then
+  rpm -q --queryformat "%{version}_%{release}" zimbra-core 
+fi
+
+if [ -f /etc/lsb-release ]; then
+  dpkg -s zimbra-core | awk -F"[ ',]+" '/Version:/{print $2}' | awk -F. '{print $1"."$2"."$3" "$4}'
+fi
